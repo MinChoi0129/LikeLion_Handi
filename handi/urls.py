bb3a90210d45aa8f027688cdbc5c3ec9ce2289c0
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2023-08-03 18:41:11 +0900

    lecture views

--- handi/urls.py
+++ handi/urls.py
@@ -4,6 +4,7 @@ from .views_lecture import *
 from .views_lecture_manager import *
 from .views_user import *
 from .views_mediaentry import *
+from .import views_lecture_manager
 
 urlpatterns = [
     path("api/translate/", Translator.as_view()),
@@ -14,18 +15,15 @@ urlpatterns = [
     path("api/user/delete/<pk>/", UserDelete.as_view()),
     path("api/users/rank/<pk>/", UserRank.as_view()),
 
-    # path("api/", LectureList.as_view()),
-    # path("api/", LectureDetail.as_view()),
-    # path("api/", LectureUpdate.as_view()),
-    # path("api/", LectureDelete.as_view()),
+    path("api/lectures/", LectureList.as_view()),
+    path("api/lecture/<pk>/", LectureDetail.as_view()),
 
     # path("api/", MediaEntryList.as_view()),
     # path("api/", MediaEntryDetail.as_view()),
     # path("api/",  MediaEntryUpdate.as_view()),
     # path("api/", MediaEntryDelete.as_view()),
 
-    # path("api/", LectureManagerList.as_view()),
-    # path("api/", LectureManagerDetail.as_view()),
-    # path("api/", LectureManagerUpdate.as_view()),
-    # path("api/", LectureManagerDelete.as_view()),
+    path("api/lecturemanager/<user_id>/", LectureManagerList.as_view()),
+    path("api/lecturemanager/<user_id>/<lecture_id>/", LectureManagerDetail.as_view()),
+    path("api/lecturemanager-update/<pk>/", LectureManagerUpdate.as_view()),
 ]
