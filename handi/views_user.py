50ad8d0bf85f760f0fa307544a83c640b2361389
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-08-03 11:47:54 +0900

    Update views_user.py

--- handi/views_user.py
+++ handi/views_user.py
@@ -30,14 +30,20 @@ class UserDelete(DestroyAPIView):
 
 class UserRank(RetrieveAPIView):
     def get(self, request, *args, **kwargs):
-        all_users = [(user.game_score, user.name)
-                     for user in User.objects.all()]
+        all_users = sorted([(user.game_score, user.name)
+                            for user in User.objects.all()], reverse=True)
         me = User.objects.get(id=kwargs['pk'])
         my_score, my_name = me.game_score, me.name
+
+        my_rank = -1
+        for i in range(len(all_users)):
+            score, name = all_users[i]
+            if score == my_score and name == my_name:
+                my_rank = i+1
+
         return JsonResponse({
-            "me": (my_score, my_name),
-            # 상위 5등만
-            "top_5_users": sorted(all_users[:5], reverse=True)
+            "me": (my_name, my_score, my_rank),
+            "top_5_users": all_users[:5]  # 상위 5등만
         }, json_dumps_params={'ensure_ascii': False})
 
     queryset = User.objects.all()
