ab974f9ab79b0bf2a9a1e5b7531a5a57ff556157
sinwoo <tlsn0613@naver.com>
2023-08-10 11:38:15 +0900

    [Feat]카드게임 구현

--- static/js/game/cardgame.js
+++ static/js/game/cardgame.js
@@ -1,10 +1,49 @@
-// JavaScript 코드
-const firstCard = document.querySelector('.FirstCard');
+const cards =document.querySelectorAll(".cards li");
 
-firstCard.addEventListener('click', function() {
-  // 클릭 시 변경하고자 하는 이미지 URL로 변경하세요.
-  const newImageUrl = '새로운 이미지 URL';
-  
-  // 이미지 변경
-  firstCard.src = newImageUrl;
-});
+        let cardOne, cardTwo; // 선택한 카드
+        let disableDeck = false;
+
+        function flipCard(e) {
+            let clickedCard = e.target;
+            
+            if(clickedCard !== cardOne && !disableDeck){ 
+                clickedCard.classList.add("flip");
+
+                if(!cardOne){
+                    return cardOne = clickedCard
+                }
+                cardTwo =clickedCard;
+                disableDeck = true;
+
+                let cardOneImg = cardOne.querySelector(".back img").src;
+                let cardTwoImg = cardTwo.querySelector(".back img").src;
+
+                matchCards(cardOneImg, cardTwoImg);
+            }
+        }
+        //두개의 이미지 비교하기
+        function matchCards(img1, img2){
+            if(img1 == img2){
+                cardOne.removeEventListener("click", flipCard);
+                cardTwo.removeEventListener("click", flipCard);
+                cardOne = cardTwo = "";
+                return disableDeck =false;
+            }else{
+                //틀린 이미지 애니메이션 효과 주기
+                setTimeout(() => {
+                    cardOne.classList.add("shake");
+                    cardTwo.classList.add("shake");
+                }, 400);
+
+                setTimeout(() => {
+                    cardOne.classList.remove("shake", "flip");
+                    cardTwo.classList.remove("shake", "flip");
+                    cardOne = cardTwo = "";
+                    return disableDeck =false;
+                }, 1200);
+            }
+        }
+
+        cards.forEach(card => {
+            card.addEventListener("click", flipCard);
+        });
