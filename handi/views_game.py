726fda89b7942e0fa89d277e59bd66871293d17d
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-08-11 22:27:54 +0900

    quiz, game 뷰 추가

--- handi/views_game.py
+++ handi/views_game.py
@@ -1,8 +1,19 @@
 from rest_framework.generics import *
 from rest_framework.response import Response
 from rest_framework import status
+from .models import MediaEntry
+from random import shuffle
 
 
 class Game(RetrieveAPIView):
     def get(self, request, *args, **kwargs):
-        return Response({}, status=status.HTTP_400_BAD_REQUEST)
+        media_entries = MediaEntry.objects.all()
+        shuffle(media_entries)
+        media_entries = media_entries[:39]
+
+        game_data = []
+        for media_entry in media_entries:
+            pair = [media_entry.name, media_entry.video_url]
+            game_data.append(pair)
+
+        return Response({"game_data": game_data}, status=status.HTTP_200_OK)
--- handi/views_quiz.py
+++ handi/views_quiz.py
@@ -2,12 +2,61 @@ from rest_framework.generics import *
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Lecture
+from random import shuffle
+from collections import deque
 
 
 class Quiz(RetrieveAPIView):
     def get(self, request, *args, **kwargs):
-        lecture = Lecture.objects.get(id=kwargs["lecture_id"])
-        quiz = []
-        for mediaentry in list(lecture.media_entries.all())[:39]:
-            quiz.append({"name": mediaentry.name, "video_url": mediaentry.video_url})
-        return Response({"quiz": quiz}, status=status.HTTP_200_OK)
+        mediaenties = Lecture.objects.get(id=kwargs["lecture_id"]).media_entries.all()
+        shuffle(mediaenties)
+        n = len(mediaenties)
+
+        mediaenties_for_type_A_quiz = deque(mediaenties[: n // 2])
+        mediaenties_for_type_B_quiz = deque(mediaenties[n // 2 :])
+
+        type_A_quizzes, type_B_quizzes = [], []
+
+        for mediaentry in mediaenties_for_type_A_quiz:
+            answer_name = mediaentry.name
+            answer_image_url = mediaentry.image_url
+
+            wrong_answer_image_urls = []
+            for _ in range(4):
+                wrong_answer = mediaenties_for_type_B_quiz.popleft()
+                wrong_answer_image_urls.append(wrong_answer.image_url)
+                mediaenties_for_type_B_quiz.append(wrong_answer)
+
+            type_A_quizzes.append(
+                {
+                    "name": answer_name,
+                    "image_urls": [answer_image_url] + wrong_answer_image_urls,
+                    # 프론트측에서 urls shuffle 해줘야함. 백엔드에서 정답은 무조건 0번 인덱스
+                }
+            )
+
+        for mediaentry in mediaenties_for_type_B_quiz:
+            answer_name = mediaentry.name
+            answer_video_url = mediaentry.video_url
+
+            wrong_answer_names = []
+            for _ in range(3):
+                wrong_answer = mediaenties_for_type_A_quiz.popleft()
+                wrong_answer_names.append(wrong_answer.name)
+                mediaenties_for_type_A_quiz.append(wrong_answer)
+
+            type_B_quizzes.append(
+                {
+                    "video_url": answer_video_url,
+                    "names": [answer_name] + wrong_answer_names,
+                    # 프론트측에서 names shuffle 해줘야함. 백엔드에서 정답은 무조건 0번 인덱스
+                }
+            )
+
+        return Response(
+            {
+                "jamo_quiz_list": mediaenties_for_type_A_quiz,
+                "word_quiz_list": mediaenties_for_type_B_quiz,
+            },
+            status=status.HTTP_200_OK,
+        )
