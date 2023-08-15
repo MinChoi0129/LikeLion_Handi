c22be99a5bc6a30e153dff7b186bc4a1b1e7eebe
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2023-08-15 14:06:42 +0900

    이미지 경로 수정

--- handi/views_lecture.py
+++ handi/views_lecture.py
@@ -6,6 +6,11 @@ from .serializers import LectureSerializer
 
 # Create a Lecuture or Get All Lectures
 class LectureList(ListCreateAPIView):
+    def get(self, request, *args, **kwargs):
+        lecture = Lecture.objects.all()
+        serializer = LectureSerializer(lecture, many=True)
+        return Response(serializer.data)
+    
     def post(self, request, *args, **kwargs):
         lecture = Lecture.objects.filter(name__contains = request.data['name'])
         serializer = LectureSerializer(lecture, many=True)
@@ -17,5 +22,10 @@ class LectureList(ListCreateAPIView):
 
 # READ Lecture
 class LectureDetail(RetrieveAPIView):
+    def get(self, request, *args, **kwargs):
+        lecture = Lecture.objects.get(id=kwargs["pk"])
+        serializer = LectureSerializer(lecture)
+        return Response(serializer.data)
+    
     queryset = Lecture.objects.all()
     serializer_class = LectureSerializer
