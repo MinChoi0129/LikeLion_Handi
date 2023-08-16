18c14ce37373db6b161b9265c77bca3f9970faa8
sinwoo <tlsn0613@naver.com>
2023-08-16 15:23:58 +0900

    [Feat] 번역페이지 ajax하는 중

--- static/js/translation.js
+++ static/js/translation.js
@@ -1,32 +1,32 @@
-// function trans() {
-//   let sentence = document.getElementById("sentence").value;
-//   fetch("http://127.0.0.1:8000/api/translate/", {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       video_url: "media/translate/1955920.mp4",
-//       mode: "download",
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((result) => {
-//       const videoUrl = result["video_url"];
-//       const videoSource = document.getElementById("videoSource");
-//       console.log(videoSource.src);
-//       console.log(videoUrl);
-//     });
-// }
-
-fetch("http://127.0.0.1:8000/api/translate/")
-  .then((response) => {
-    return response.json();
+function trans() {
+  let sentence = document.getElementById("sentence").value;
+  fetch("http://127.0.0.1:8000/api/translate/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      video_url: "media/translate/7176006.mp4",
+      mode: "download",
+    }),
   })
-  .then((result) => {
-    const videoUrl = result["video_url"];
-    const videoSource = document.getElementById("videoSource");
-    videoSource.src = videoUrl;
-    console.log(result["video_url"]);
-  });
+    .then((response) => response.json())
+    .then((result) => {
+      const videoUrl = result["video_url"];
+      const videoSource = document.getElementById("videoSource");
+      console.log(videoSource.src);
+      console.log(videoUrl);
+    });
+}
+
+// fetch("http://127.0.0.1:8000/api/translate/")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((result) => {
+//     const videoUrl = result["video_url"];
+//     const videoSource = document.getElementById("videoSource");
+//     videoSource.src = videoUrl;
+//     console.log(result["video_url"]);
+//   });
