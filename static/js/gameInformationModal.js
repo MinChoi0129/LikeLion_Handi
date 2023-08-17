40dd97111a65e9d3fdd0ab4e5abcd34b374f93b5
MinJae Lee <wjdchs0129@gnu.ac.kr>
2023-08-17 14:03:41 +0900

    url 수정완료

--- static/js/gameInformationModal.js
+++ static/js/gameInformationModal.js
@@ -3,6 +3,8 @@ const openButton = document.getElementById("open");
 const modal = document.querySelector(".modal");
 const overlay = modal.querySelector(".modalOverlay");
 const closeBtn = document.querySelector("#closeBtn");
+const startBtn = document.querySelector(".startBtn");
+
 
 const openModal = () => {
   modal.classList.remove("hidden");
@@ -12,6 +14,11 @@ const closeModal = () => {
   modal.classList.add("hidden");
 };
 
+const startGame = () => {
+  window.location.href = "http://127.0.0.1:8000/game/start/"
+}
+
 overlay.addEventListener("click", closeModal);
 closeBtn.addEventListener("click", closeModal);
 openButton.addEventListener("click", openModal);
+startBtn.addEventListener("click", startGame);
