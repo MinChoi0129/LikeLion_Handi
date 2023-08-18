8d8b91ee0c5d111a907f8e1f2b79f2ef9ee3b8d1
jhk3261 <jhk3261@gnu.ac.kr>
2023-08-18 17:51:44 +0900

    result수정

--- static/js/testWord.js
+++ static/js/testWord.js
@@ -7,6 +7,24 @@ const BackStageBar = document.querySelector('.BackStageBar');
 const QuizWrap = document.querySelector('.QuizWrap');
 const StopBtn = document.querySelector('.StopBtnBox');
 
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
 // 퀴즈 시작 전 모달
 const Back = document.querySelector('.Back');
 const StartModal = document.querySelector('.StartModal');
@@ -119,6 +137,20 @@ fetch('http://127.0.0.1:8000/api/quiz/' + Id + "/")
                 RightPer = Math.round((count - wrong_choices.length) / count * 100);
                 console.log(RightPer);
                 // wrong_choices, RightPer 보내기
+                fetch("url만들면넣기" + Id + "/", {
+                    method: "PATCH",
+                    credentials: 'include',
+                    headers: {
+                        "Content-Type": 'application/x-www-form-urlencoded',
+                        'X-CSRFToken': csrftoken,
+                    },
+                    cache: 'no-cache',
+                    mode: 'same-origin',
+                    body: new URLSearchParams({파라미터: RightPer, wrong_choices})
+                })
+                .then((data) => {
+                    console.log(data)
+                })
                 location.replace("http://127.0.0.1:8000/lecture/" + Id + "/result/word");
                 return
             }
@@ -128,6 +160,20 @@ fetch('http://127.0.0.1:8000/api/quiz/' + Id + "/")
                 RightPer = Math.round((count - wrong_choices.length) / count * 100);
                 console.log(RightPer);
                 // wrong_choices, RightPer 보내기
+                fetch("url만들면넣기" + Id + "/", {
+                    method: "PATCH",
+                    credentials: 'include',
+                    headers: {
+                        "Content-Type": 'application/x-www-form-urlencoded',
+                        'X-CSRFToken': csrftoken,
+                    },
+                    cache: 'no-cache',
+                    mode: 'same-origin',
+                    body: new URLSearchParams({파라미터: RightPer, wrong_choices})
+                })
+                .then((data) => {
+                    console.log(data)
+                })
                 location.replace("http://127.0.0.1:8000/lecture/" + Id + "/result/word");
                 return
             }
